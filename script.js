4ad7ce0be2f8101a31e268cfd158ea30008bc5b8
Yaphets77 <119989223+Yaphets77@users.noreply.github.com>
2023-01-27 10:21:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+var block = document.getElementById("block");
+var hole = document.getElementById("hole");
+var character = document.getElementById("character");
+var jumping = 0;
+var counter = 0;
+
+
+hole.addEventListener('animationiteration', () => {
+    var random = -((Math.random()*300)+150);
+    hole.style.top = random + "px";
+    counter++;
+});
+setInterval(function(){
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    if(jumping==0){
+        character.style.top = (characterTop+3)+"px";
+    }
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+    var cTop = -(500-characterTop);
+    if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop+130)))){
+        alert("Game over. Score: "+(counter-1));
+        character.style.top = 100 + "px";
+        counter=0;
+    }   else if((blockLeft<20)&&(blockLeft>-50)&&((cTop>holeTop)&&(cTop<holeTop+130))){
+        var scoreSound = document.getElementById("scoreSound");
+        scoreSound.play();
+        counter++;
+    }
+},10);
+
+
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 32) {
+      jump();
+    }
+  });
+  
+
+
+function jump(){
+    jumping = 1;
+    let jumpCount = 0;
+    var jumpInterval = setInterval(function(){
+        var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        if((characterTop>6)&&(jumpCount<15)){
+            character.style.top = (characterTop-5)+"px";
+        }
+        if(jumpCount>20){
+            clearInterval(jumpInterval);
+            jumping=0;
+            jumpCount=0;
+        }
+        jumpCount++;
+    },10);
+    var jumpSound = document.getElementById("jumpSound");
+    jumpSound.play();
+      
+}
+
+
